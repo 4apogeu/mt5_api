a4d91ce37dbda1132a6651dfae10d4bcc2772433
MyContrib <h.bui.tech@gmail.com>
2025-12-28 15:20:31 +0700

    feat: export new UI widgets and utilities from mt5_bridge.ui

--- mt5_bridge/ui/__init__.py
+++ mt5_bridge/ui/__init__.py
@@ -1,5 +1,36 @@
 """Order Panel UI for MT5 Bridge."""
 
 from .order_panel import OrderPanel
+from .config import PanelConfig, Colors, DEFAULT_CONFIG, DEFAULT_COLORS
+from .widgets import (
+    TradeButton, StatusBar, SymbolSelector, VolumeInput,
+    ModeInput, SLTPMode, PositionRow, PositionsPanel,
+)
+from .utils import (
+    OrderSide, SLTPMode as SLTPModeUtil,
+    calculate_sl_price, calculate_tp_price, calculate_trailing_sl,
+)
+from .trailing_stop import TrailingStopManager, TrailingState
 
-__all__ = ["OrderPanel"]
+__all__ = [
+    "OrderPanel",
+    "PanelConfig",
+    "Colors",
+    "DEFAULT_CONFIG",
+    "DEFAULT_COLORS",
+    "TradeButton",
+    "StatusBar",
+    "SymbolSelector",
+    "VolumeInput",
+    "ModeInput",
+    "SLTPMode",
+    "PositionRow",
+    "PositionsPanel",
+    "OrderSide",
+    "SLTPModeUtil",
+    "calculate_sl_price",
+    "calculate_tp_price",
+    "calculate_trailing_sl",
+    "TrailingStopManager",
+    "TrailingState",
+]
